8a9e4fa318e8217d4843c873d2a1839857d7b835
Ezequiel Gonçalves <ezekiel_sci@hotmail.com>
2020-09-22 00:23:43 -0300

    Making relations between orders and customers

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,4 +1,4 @@
-import { getRepository, Repository, In } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
@@ -21,21 +21,50 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productsByIds = await this.ormRepository.findByIds(products);
+
+    return productsByIds;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productsIds = products.map(item => item.id);
+
+    const findProductsByIds = await this.ormRepository.findByIds(productsIds);
+
+    findProductsByIds.map(item => {
+      const product = item;
+      const index = productsIds.indexOf(item.id);
+      if (index) {
+        product.quantity = products[index].quantity;
+      }
+      return product;
+    });
+
+    return findProductsByIds;
   }
 }
 
